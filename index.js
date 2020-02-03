d327e8cf54a142649fcfff0a925ddc6ff34e6737
Grace Shih <vocalise@gmail.com>
2020-02-03 00:26:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ function wrapAdjective(flair="*") {
 }
 
 const Calculator = {
-<<<<<<< HEAD
    add : function (num1, num2) {
     return num1 + num2;
   },
@@ -29,30 +28,13 @@ const Calculator = {
   }
 };
 
-function actionApplyer(strt_integer, array_of_functions) {
+function actionApplyer(strt_integer, array_of_functions){
   if (!Array.isArray(array_of_functions) || (!array_of_functions.length)) {
     return strt_integer;
   } else {
     return array_of_functions[2](array_of_functions[1](array_of_functions[0](strt_integer, 2)));
-=======
     this.add = function(num1, num2) {
-    return num1 + num2;
+      return num1 + num2;
+    }
   }
 };
-  
-function Calculator2() {
-
-  
-  function subtract(num1, num2) {
-    return num1 - num3;
-  }
-  
-  function multiply(num1, num2) {
-    return num1 * num2;
-  }
-  
-  function divide(num1, num2) {
-    return num1 / num2;
->>>>>>> 902d5647a0cd6498d4fb4ff2fbfa49e5570f1509
-  }
-}
